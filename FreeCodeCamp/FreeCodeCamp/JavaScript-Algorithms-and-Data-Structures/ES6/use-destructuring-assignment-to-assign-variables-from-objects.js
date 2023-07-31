b08585beb4ccdbf83835fd8e8ccf29ef64eb79a0
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-07-30 22:38:40 -0700

    2 files [2023.07.29-2023.07.30]
    2 files [2023.07.29-2023.07.30]

---
+++ FreeCodeCamp/FreeCodeCamp/JavaScript-Algorithms-and-Data-Structures/ES6/use-destructuring-assignment-to-assign-variables-from-objects.js
@@ -0,0 +1,19 @@
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+  
+// const highToday = HIGH_TEMPERATURES.today;
+// const highTomorrow = HIGH_TEMPERATURES.tomorrow; 
+
+const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES
+
+// const { highToday } = HIGH_TEMPERATURES.today
+// const { highTomorrow } = HIGH_TEMPERATURES.tomorrow
+
+// const highTomorrow = HIGH_TEMPERATURES.tomorrow; 
+
+// Only change code above this line
---
+++ FreeCodeCamp/FreeCodeCamp/JavaScript-Algorithms-and-Data-Structures/ES6/use-destructuring-assignment-to-extract-values-from-objects.js
@@ -0,0 +1,32 @@
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+/*
+const today = HIGH_TEMPERATURES.today;
+const tomorrow = HIGH_TEMPERATURES.tomorrow;
+*/
+
+/*
+const today = { HIGH_TEMPERATURES, today }
+const tomorrow = { HIGH_TEMPERATURES, tomorrow }
+*/
+
+// const { HIGH_TEMPERATURES } = today;
+/*
+const today = { HIGH_TEMPERATURES }
+const tomorrow = { HIGH_TEMPERATURES }
+*/
+
+const { today, tomorrow } = HIGH_TEMPERATURES
+
+// const { HIGH_TEMPERATURES.tomorrow} = tomorrow;
+
+console.log(HIGH_TEMPERATURES.today)
+console.log(HIGH_TEMPERATURES.tomorrow)
+
+// Only change code above this line
